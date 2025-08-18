c6dca7a4e9cb8cfef61eca41adbd92604e60a524
Jitendra Prajapati <jituprajapati834@gmail.com>
2025-08-18 16:59:50 +0000

    9

---
+++ ArrayBasic/Pro9.java
@@ -0,0 +1,31 @@
+
+
+// Write a Java program to display all even numbers and odd numbers separately from an array.
+
+import java.util.Scanner;
+
+public class Pro9 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter size of array: ");
+        int n = sc.nextInt();
+
+        int arr[] = new int[n];
+        System.out.println("Enter " + n + " elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        System.out.print("Even numbers: ");
+        for (int i = 0; i < n; i++) {
+            if (arr[i] % 2 == 0) {
+                System.out.print(arr[i] + " ");
+            }
+        }
+        System.out.print("\nOdd numbers: ");
+        for (int i = 0; i < n; i++) {
+            if (arr[i] % 2 != 0) {
+                System.out.print(arr[i] + " ");
+            }
+        }
+    }
+}
